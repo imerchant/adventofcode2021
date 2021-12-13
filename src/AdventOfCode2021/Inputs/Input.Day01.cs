31fa4b7d47225ce9406e6e45a0664ee0157e2802
Imran Merchant <imerchant@outlook.com>
2021-12-12 21:27:07 -0600

    Add project skeleton

---
+++ src/AdventOfCode2021/Inputs/Input.Day01.cs
@@ -0,0 +1,6 @@
+namespace AdventOfCode2021.Inputs;
+
+public partial class Input
+{
+    public const string Day01 = @"";
+}
---
+++ test/AdventOfCode2021.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace AdventOfCode2021.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
