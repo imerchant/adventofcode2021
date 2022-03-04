9ba5594c9e4bf47ec0ed2c2d691b87b02f9702eb
Imran Merchant <imerchant@outlook.com>
2022-03-03 22:57:11 -0600

    Add Day08 solution 2

--- src/AdventOfCode2021/Day08/Display.cs
+++ src/AdventOfCode2021/Day08/Display.cs
@@ -27,14 +27,67 @@ public class Entry
 {
     public List<Segment> Input { get; }
     public List<Segment> Output { get; }
+    public int Value { get; } = -1;
 
-    public int InstancesOf1478 => Output.Count(x => x.Value.Length is 2 or 4 or 3 or 7);
+    public int InstancesOf1478 => Output.Count(x => x.Length is 2 or 4 or 3 or 7);
 
     public Entry(string input, string output)
     {
         Input = input.SplitOn(' ').Select(x => new Segment(x)).ToList();
         Output = output.SplitOn(' ').Select(x => new Segment(x)).ToList();
+
+        var one = Input.Single(x => x.Length == 2).SetValue(1);
+        var four = Input.Single(x => x.Length == 4).SetValue(4);
+        var seven = Input.Single(x => x.Length == 3).SetValue(7);
+        var eight = Input.Single(x => x.Length == 7).SetValue(8);
+
+        var three = Input.Single(x => x.Length == 5 && one.IsSubsetOf(x)).SetValue(3);
+
+        var fourLessOne = four.Minus(one.Letters);
+        var zero = Input.Single(x => x.Length == 6 && !fourLessOne.IsSubsetOf(x.Letters)).SetValue(0);
+
+        var nine = Input.Single(x => x.Length == 6 && x.Value == -1 && one.IsSubsetOf(x)).SetValue(9);
+
+        var six = Input.Single(x => x.Length == 6 && x.Value == -1).SetValue(6);
+
+        var five = Input.Single(x => x.Length == 5 && x.Minus(fourLessOne).Count == 3).SetValue(5);
+
+        var two = Input.Single(x => x.Value == -1).SetValue(2);
+
+        foreach (var segment in Output)
+        {
+            segment.SetValue(Input.Single(x => x.Letters.SetEquals(segment.Letters)).Value);
+        }
+
+        Value = int.Parse(Output.Select(x => x.Value).JoinStrings());
     }
 }
 
-public record Segment(string Value);
+public class Segment
+{
+    public string Content { get; }
+    public int Length => Content.Length;
+    public HashSet<char> Letters { get; }
+    public int Value { get; private set; } = -1;
+
+    public Segment(string content)
+    {
+        Content = content;
+        Letters = new HashSet<char>(Content);
+    }
+
+    public Segment SetValue(int value)
+    {
+        Value = value;
+        return this;
+    }
+
+    public bool IsSubsetOf(Segment other) => Letters.IsSubsetOf(other.Letters);
+
+    public HashSet<char> Minus(HashSet<char> other)
+    {
+        var letters = new HashSet<char>(Letters);
+        letters.ExceptWith(other);
+        return letters;
+    }
+}
--- test/AdventOfCode2021.Tests/Day08Solutions.cs
+++ test/AdventOfCode2021.Tests/Day08Solutions.cs
@@ -5,14 +5,15 @@ namespace AdventOfCode2021.Tests;
 public class Day08Solutions
 {
     [Fact]
-    public void Puzzle1_Count1478Instances()
+    public void Puzzle1And2_Count1478Instances_AndFindSumOfOutput()
     {
         var display = new Display(Input.Day08);
         
         display.Sum(x => x.InstancesOf1478).Should().Be(397);
+        display.Sum(x => x.Value).Should().Be(1027422);
     }
     
-    public const string Puzzle1Example = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    public const string PuzzleExample = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -24,10 +25,61 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
 
     [Fact]
-    public void Puzzle1Example_Count1478Instances()
+    public void Puzzle1_ExampleCounts1478Instances()
     {
-        var display = new Display(Puzzle1Example);
+        var display = new Display(PuzzleExample);
 
         display.Sum(x => x.InstancesOf1478).Should().Be(26);
+    }
+
+    [Fact]
+    public void Puzzle2Example_EntryAssignsCorrectValues()
+    {
+        const string input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab";
+        const string output = "cdfeb fcadb cdfeb cdbaf";
+        const string threeSegment = "fbcad";
+        const string zeroSegment = "cagedb";
+        const string nineSegment = "cefabd";
+        const string sixSegment = "cdfgeb";
+        const string fiveSegment = "cdfbe";
+        const string twoSegment = "gcdfa";
+
+        var entry = new Entry(input, output);
+
+        entry.Input.First(x => x.Content == threeSegment).Value.Should().Be(3);
+        entry.Input.First(x => x.Content == zeroSegment).Value.Should().Be(0);
+        entry.Input.First(x => x.Content == nineSegment).Value.Should().Be(9);
+        entry.Input.First(x => x.Content == sixSegment).Value.Should().Be(6);
+        entry.Input.First(x => x.Content == fiveSegment).Value.Should().Be(5);
+        entry.Input.First(x => x.Content == twoSegment).Value.Should().Be(2);
+
+        entry.Output.Should().SatisfyRespectively(
+            x => x.Value.Should().Be(5),
+            x => x.Value.Should().Be(3),
+            x => x.Value.Should().Be(5),
+            x => x.Value.Should().Be(3));
+
+        entry.Value.Should().Be(5353);
+    }
+
+    [Fact]
+    public void Puzzle2_ExampleGathersOutputValues()
+    {
+        var display = new Display(PuzzleExample);
+
+        display.Should().SatisfyRespectively(
+            x => x.Value.Should().Be(8394),
+            x => x.Value.Should().Be(9781),
+            x => x.Value.Should().Be(1197),
+            x => x.Value.Should().Be(9361),
+            x => x.Value.Should().Be(4873),
+            x => x.Value.Should().Be(8418),
+            x => x.Value.Should().Be(4548),
+            x => x.Value.Should().Be(1625),
+            x => x.Value.Should().Be(8717),
+            x => x.Value.Should().Be(4315)
+        );
+
+        display.Sum(x => x.Value).Should().Be(61229);
     }
 }
