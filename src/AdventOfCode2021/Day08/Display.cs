beea28fcc729f297802cf57766cc4e8b80b05c8a
Imran Merchant <imerchant@outlook.com>
2022-03-03 21:36:11 -0600

    Add Day08 solution 1

---
+++ src/AdventOfCode2021/Day08/Display.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace AdventOfCode2021.Day08;
+
+public class Display : IEnumerable<Entry>
+{
+    public List<Entry> Entries { get; }
+
+    public Display(string input)
+    {
+        Entries = input
+            .SplitLines()
+            .Select(line =>
+            {
+                var contents = line.SplitOn('|').TrimEach().ToList();
+                return new Entry(contents[0], contents[1]);
+            })
+            .ToList();
+    }
+
+    public IEnumerator<Entry> GetEnumerator() => Entries.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class Entry
+{
+    public List<Segment> Input { get; }
+    public List<Segment> Output { get; }
+
+    public int InstancesOf1478 => Output.Count(x => x.Value.Length is 2 or 4 or 3 or 7);
+
+    public Entry(string input, string output)
+    {
+        Input = input.SplitOn(' ').Select(x => new Segment(x)).ToList();
+        Output = output.SplitOn(' ').Select(x => new Segment(x)).ToList();
+    }
+}
+
+public record Segment(string Value);
