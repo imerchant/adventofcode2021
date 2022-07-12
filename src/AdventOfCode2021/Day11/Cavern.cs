ea1a65a0ee3a57ba9ef3938e0037335d85847ade
Imran Merchant <imerchant@outlook.com>
2022-07-11 19:34:14 -0500

    Add Day 11 solution 1

---
+++ src/AdventOfCode2021/Day11/Cavern.cs
@@ -0,0 +1,104 @@
+﻿using System.Text;
+
+namespace AdventOfCode2021.Day11;
+
+public class Cavern
+{
+    private static readonly List<(int X, int Y)> AdjacentMods = new List<(int X, int Y)>
+    {
+        (-1, -1),
+        ( 0, -1),
+        ( 1, -1),
+        ( 1,  0),
+        ( 1,  1),
+        ( 0,  1),
+        (-1,  1),
+        (-1,  0)
+    };
+
+    public IDictionary<(int X, int Y), int> Octopuses { get; }
+    public int Height { get; }
+    public int Width { get; }
+
+    public Cavern(string input)
+    {
+        var lines = input.SplitLines().ToList();
+
+        Height = lines.Count;
+        Width = lines[0].Length;
+        Octopuses = new Dictionary<(int X, int Y), int>();
+
+        for (var k = 0; k < Height; ++k)
+        {
+            for (var j = 0; j < Width; j++)
+            {
+                Octopuses[(k, j)] = lines[k][j] - '0';
+            }
+        }
+    }
+
+    public int Step()
+    {
+        var flashes = new HashSet<(int X, int Y)>();
+
+        foreach (var key in Octopuses.Keys)
+        {
+            Octopuses[key]++;
+            if (Octopuses[key] > 9)
+            {
+                flashes.Add(key);
+            }
+        }
+
+        var traversing = new Stack<(int X, int Y)>(flashes);
+
+        while (traversing.TryPeek(out _))
+        {
+            var position = traversing.Pop();
+            foreach (var adjacent in GetAdjacents(position))
+            {
+                if (++Octopuses[adjacent] > 9 && flashes.Add(adjacent))
+                {
+                    traversing.Push(adjacent);
+                }
+            }
+        }
+
+        foreach (var flashed in flashes)
+        {
+            Octopuses[flashed] = 0;
+        }
+
+        return flashes.Count;
+
+        IEnumerable<(int X, int Y)> GetAdjacents((int X, int Y) point)
+        {
+            foreach (var (modX, modY) in AdjacentMods)
+            {
+                var (newX, newY) = (point.X + modX, point.Y + modY);
+                if (newX >= 0 && newX < Width &&
+                    newY >= 0 && newY < Height)
+                {
+                    yield return (newX, newY);
+                }
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+
+        for (var row = 0; row < Height; row++)
+        {
+            for (var col = 0; col < Width; col++)
+            {
+                builder.Append(Octopuses[(row, col)]);
+            }
+
+            builder.AppendLine();
+        }
+
+        return builder.ToString().Trim();
+    }
+}
---
+++ test/AdventOfCode2021.Tests/Day11Solutions.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using AdventOfCode2021.Day11;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2021.Tests;
+
+public class Day11Solutions
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day11Solutions(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Puzzle1_CountFlashesAfter100Steps()
+    {
+        var cavern = new Cavern(Input.Day11);
+
+        var flashes = 0;
+        for (var k = 0; k < 100; ++k)
+        {
+            flashes += cavern.Step();
+        }
+
+        _output.WriteLine("After step 100:");
+        _output.WriteLine(cavern.ToString());
+
+        flashes.Should().Be(1546);
+    }
+
+
+    public const string PuzzleExample =
+@"5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+    public const string SmallExample =
+@"11111
+19991
+19191
+19991
+11111";
+
+    [Theory]
+    [MemberData(nameof(PrintCavernCases))]
+    public void PrintExampleCavern(string example)
+    {
+        var cavern = new Cavern(example);
+
+        _output.WriteLine(cavern.ToString());
+
+        cavern.ToString().Should().Be(example);
+    }
+
+    public static IEnumerable<object[]> PrintCavernCases()
+    {
+        yield return new object[] { SmallExample };
+        yield return new object[] { PuzzleExample };
+    }
+
+    [Fact]
+    public void SmallExample_FlashesAsExpected()
+    {
+        var cavern = new Cavern(SmallExample);
+
+        _output.WriteLine("Before any steps:");
+        _output.WriteLine(cavern.ToString());
+        _output.WriteLine(string.Empty);
+
+        cavern.Step();
+
+        _output.WriteLine("After step 1:");
+        _output.WriteLine(cavern.ToString());
+        _output.WriteLine(string.Empty);
+
+        cavern.Step();
+
+        _output.WriteLine("After step 2:");
+        _output.WriteLine(cavern.ToString());
+    }
+
+    [Fact]
+    public void PuzzleExample_FlashesAsExpected()
+    {
+        var cavern = new Cavern(PuzzleExample);
+
+        _output.WriteLine("Before any steps:");
+        _output.WriteLine(cavern.ToString());
+        _output.WriteLine(string.Empty);
+
+        cavern.Step();
+
+        _output.WriteLine("After step 1:");
+        _output.WriteLine(cavern.ToString());
+        _output.WriteLine(string.Empty);
+
+        cavern.Step();
+
+        _output.WriteLine("After step 2:");
+        _output.WriteLine(cavern.ToString());
+    }
+
+    [Theory]
+    [InlineData(10, 204)]
+    [InlineData(100, 1656)]
+    public void PuzzleExample_CountFlashesForXSteps(int steps, int expectedFlashes)
+    {
+        var cavern = new Cavern(PuzzleExample);
+
+        var flashes = 0;
+        for (var k = 0; k < steps; ++k)
+        {
+            flashes += cavern.Step();
+        }
+
+        _output.WriteLine($"After step {steps}:");
+        _output.WriteLine(cavern.ToString());
+
+        flashes.Should().Be(expectedFlashes);
+    }
+}
